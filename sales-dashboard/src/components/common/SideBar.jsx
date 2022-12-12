237da7874791ea80e47b405686a5e7ab69169281
Altan SOZUER <sozueraltan@gmail.com>
2022-12-12 22:44:39 +0300

    SideBar is refactored according to ListItemComposition

--- sales-dashboard/src/components/common/SideBar.jsx
+++ sales-dashboard/src/components/common/SideBar.jsx
@@ -1,85 +1,48 @@
-import { Box,  List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material";
+import { Box,  List} from "@mui/material";
 import {
-    AccountBox,
-    Article,
     Group,
     Home,
-    ModeNight,
-    Person,
     Settings,
     Storefront,
+    AttachMoney,
+    Store
   } from "@mui/icons-material";
+import ListItemComposition from "../organisms/ListItemComposition";
+
+
 export default function SideBar(props) {
-    return (
-        <Box bgcolor="#267" flex={1} p={2} sx={{ height: "calc(100vh - 50px )" ,display: { xs: "none", sm: "block" } }}>
-          <Box position="fixed">
-            <List>
-              <ListItem disablePadding >
-                <ListItemButton component="a" href="#home" >
-                  <ListItemIcon>
-                    <Home />
-                  </ListItemIcon>
-                  <ListItemText primary="Homepage" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <Article />
-                  </ListItemIcon>
-                  <ListItemText primary="Pages" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <Group />
-                  </ListItemIcon>
-                  <ListItemText primary="Groups" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <Storefront />
-                  </ListItemIcon>
-                  <ListItemText primary="Marketplace" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <Person />
-                  </ListItemIcon>
-                  <ListItemText primary="Friends" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <Settings />
-                  </ListItemIcon>
-                  <ListItemText primary="Settings" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <AccountBox />
-                  </ListItemIcon>
-                  <ListItemText primary="Profile" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton component="a" href="#simple-list">
-                  <ListItemIcon>
-                    <ModeNight />
-                  </ListItemIcon>
-                  <Switch />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Box>
+  return (
+      <Box bgcolor={"aliceblue"} flex={1} p={2} sx={{ height: "calc(100vh - 50px )" ,display: { xs: "none", sm: "block" } }}>
+        <Box position="fixed">
+          <List >
+            <ListItemComposition to="/"
+                  icon= { <Home />}
+                  text="Homepage"
+                  disablePadding={true} />
+            <ListItemComposition to="/user"
+                  icon= { <Group />}
+                  text="Users"
+                  disablePadding={true} />
+            <ListItemComposition to="/product"
+                  icon= { <Store />}
+                  text="Products"
+                  disablePadding={true} />
+            <ListItemComposition to="/sales"
+                  icon= { <Storefront  />}
+                  text="Sales"
+                  disablePadding={true} />
+            <ListItemComposition to="/transaction"
+                  icon= { <AttachMoney />}
+                  text="Transactions"
+                  disablePadding={true} />
+
+            <ListItemComposition to="/settings"
+                  icon= { <Settings />}
+                  text="Settings"
+                  disablePadding={true} />
+            
+          </List>
         </Box>
-    )
+      </Box>
+  )
 }
