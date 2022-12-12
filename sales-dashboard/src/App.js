a25ed1d3309d6321b3f78ba11a8e40f523d37234
Altan SOZUER <sozueraltan@gmail.com>
2022-12-12 22:42:18 +0300

    Browser route is configured with pages.

--- sales-dashboard/src/App.js
+++ sales-dashboard/src/App.js
@@ -2,15 +2,32 @@ import TopBar from "./components/common/TopBar";
 import { Box, Stack } from "@mui/material";
 import SideBar from "./components/common/SideBar";
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from 'react-router-dom'
+import Products from "./pages/Products";
+import Users from "./pages/Users";
+import Transactions from "./pages/Transactions";
+import Sales from "./pages/Sales";
+import Settings from "./pages/Settings";
+import ErrorPage from "./pages/ErrorPage";
 function App() {
   return (
     <Box>
-      <TopBar />
-      <Stack direction="row" spacing={1} justifyContent="space-between">
-        <SideBar />
-        <HomePage />
-      </Stack>
-    </Box>
+        <TopBar />
+        <Stack direction="row" spacing={1} justifyContent="space-between">
+          <SideBar />
+          <Box sx={{ flex: 4 }}>
+            <Routes>
+              <Route path={"/"} element={ <HomePage />} />
+              <Route path={"/user"} element={ <Users />} />
+              <Route path={"/product"} element={ <Products />} />
+              <Route path={"/transaction"} element={ <Transactions />} />
+              <Route path={"/sales"} element={ <Sales />} />
+              <Route path={"/settings"} element={ <Settings />} />
+              <Route path="*" element={ <ErrorPage />} />
+            </Routes>
+          </Box>
+        </Stack>
+      </Box>
   );
 }
 
