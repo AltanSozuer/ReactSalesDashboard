9db8fa482a98cb9d6d50559d6a3c787995f8fded
Altan SOZUER <sozueraltan@gmail.com>
2022-12-14 14:57:44 +0300

    LineChart that will be shown in homepage and its css file are created

---
+++ sales-dashboard/src/components/organisms/LineChart.jsx
@@ -0,0 +1,29 @@
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { ACTIVE_USER_COUNT } from "../../DummyData/MonthlyActiveUser"
+import "../../styles/css/chart.css"
+
+export default function LineCharts(props){
+    return (
+        <div className='chart'>
+            <h3 className='chartTitle'>Monthly Active Users</h3>
+            <ResponsiveContainer width="100%" aspect={ 5 / 2 }>
+                <LineChart
+                    width={1000}
+                    height={400}
+                    data={ACTIVE_USER_COUNT}
+                    margin={{
+                        top: 10,
+                        right: 20,
+                        left: 20,
+                        bottom: 10,
+                    }} >
+                    <CartesianGrid strokeDasharray="5 5" />
+                    <XAxis dataKey="month" />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="Active User" stroke="#8884d8" strokeWidth={2} />
+                </LineChart>    
+            </ResponsiveContainer>
+        </div>
+    );
+}
+  
