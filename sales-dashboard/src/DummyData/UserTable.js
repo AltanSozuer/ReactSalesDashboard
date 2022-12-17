23d0fa746a070051ef5d4cf7e167b4d82a09cdba
Altan SOZUER <sozueraltan@gmail.com>
2022-12-17 19:50:59 +0300

    Users page and its components are created

---
+++ sales-dashboard/src/DummyData/UserTable.js
@@ -0,0 +1,74 @@
+export const userData = [
+        { 
+            id: 1, 
+            fullName: 'Jon Snow', 
+            email: "jonsnow@gmail.com", 
+            status: "Active", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 2, 
+            fullName: 'Cersei Lannister', 
+            email: "cersei@gmail.com", 
+            status: "Inactive", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 3, 
+            fullName: 'Jaime Lannister', 
+            email: "jaime@gmail.com", 
+            status: "Active", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 4, 
+            fullName: 'Arya Stark', 
+            email: "starkArya@gmail.com", 
+            status: "Active", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 5, 
+            fullName: 'Daenerys Targaryen', 
+            email: "targaryen@gmail.com", 
+            status: "Active", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 6, 
+            fullName: 'Adam Melisandre', 
+            email: "melisandre@gmail.com", 
+            status: "Inactive", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 7, 
+            fullName: 'Ferrara Clifford', 
+            email: "clifford@gmail.com", 
+            status: "Active", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 8, 
+            fullName: 'Rossini Frances', 
+            email: "frances@gmail.com", 
+            status: "Inactive", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        },
+        { 
+            id: 9, 
+            fullName: 'Harvey Roxie', 
+            email: "roxie@gmail.com", 
+            status: "Inactive", 
+            total_transaction: "$350", 
+            image: "https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        }
+]
--- sales-dashboard/src/pages/Users.jsx
+++ sales-dashboard/src/pages/Users.jsx
@@ -1,9 +1,108 @@
-import { Box } from "@mui/material";
+import "../styles/css/shared/shadowbox.css"
+import "../styles/css/userTable.css"
+import { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { Chip, Tooltip, Zoom } from '@mui/material';
+import { Edit, Delete } from "@mui/icons-material"
+import { userData } from '../DummyData/UserTable';
+import AtomIconButton from "../components/atoms/AtomIconButton"
+import { Link } from "react-router-dom"
 
-export default function Users(props) {
-    return (
-        <Box >
-           Users
-        </Box>
-    )
+function labelTypeDetector(status) {
+  return (status === "Active") ? "success" : "warning"; 
+}
+
+
+export default function Users() {
+  const [ data, setData ] = useState(userData);
+
+  const handleDeleteAction = ( id ) => {
+    setData( data.filter( obj => obj.id !== id ))
+  }
+
+  const columns = [
+    { 
+      field: 'id',
+      headerName: 'ID', 
+      width: 80,
+    },
+    { 
+      field: 'fullName', 
+      headerName: 'Full Name', 
+      width: 250,
+      renderCell: ( params ) => {
+        return (
+          <div className='fullname'>
+            <img src= { params.row.image } className="fullname-image" alt="" />
+            { params.row.fullName }
+          </div>
+        )
+      }
+         
+    },
+    { 
+      field: 'email', 
+      headerName: 'Email', 
+      width: 200 
+    },
+    { 
+      field: 'status', 
+      headerName: 'Status', 
+      width: 120 ,
+      renderCell: ( params ) => {
+        return (
+          <Chip 
+            label={ params.row.status } 
+            color={labelTypeDetector(params.row.status)}
+            sx={{ padding: "5px"}} /> 
+        )
+      }
+    },
+    {
+      field: 'total_transaction',
+      headerName: 'Total Transaction',
+      width: 200,
+    },
+    {
+      field: 'action',
+      headerName: 'Actions',
+      width: 150,
+      renderCell: ( params ) => {
+        return (
+          <div className='action-container'>
+              <Tooltip title="Edit" TransitionComponent={ Zoom }>
+                <Link to={`/user/${params.row.id}`}>
+                  <AtomIconButton text="" size="small" color= "success"> 
+                    <Edit />
+                  </AtomIconButton>
+                </Link>
+              </Tooltip>
+              
+              <Tooltip title="Delete" TransitionComponent={ Zoom } >
+                <span>
+                <AtomIconButton text="" size="small" onClick={() => handleDeleteAction(params.row.id)} color= "error" sx={{ marginRight: "15px" }}> 
+                  <Delete />
+                </AtomIconButton>
+                </span>
+              </Tooltip>
+              
+          </div> 
+        )
+      }
+    }
+  ];
+
+  return (
+    <div className='shadow-box' 
+         style={{ height: "80%", width: '95%', margin: "20px 10px" }}>
+      <DataGrid
+        rows={data}
+        columns={ columns }
+        pageSize={10}
+        autoPageSize
+        density="comfortable" 
+        disableSelectionOnClick
+        checkboxSelection />
+    </div>
+  );
 }
