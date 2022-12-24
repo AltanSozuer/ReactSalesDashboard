3ba9ec472a5d6f8429e810e654df533184abfbb4
Altan SOZUER <sozueraltan@gmail.com>
2022-12-24 21:03:04 +0300

    Revert "Revert "newProduct route is added to main route""
    This reverts commit 0cc6e3a1eacd090857190f3aad69da661a85b44b.

--- sales-dashboard/src/components/organisms/LineChart.jsx
+++ sales-dashboard/src/components/organisms/LineChart.jsx
@@ -1,30 +1,51 @@
-import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import { ACTIVE_USER_COUNT } from "../../DummyData/MonthlyActiveUser"
 import "../../styles/css/chart.css"
 import "../../styles/css/shared/shadowbox.css"
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import PropTypes from 'prop-types'
 
-export default function LineCharts(props){
+export default function LineCharts({
+    title,
+    data,
+    width,
+    height,
+    chartMargin,
+    lineDataKey,
+    xAxisDataKey
+}){
     return (
         <div className='chart shadow-box' >
-            <h3 className='chartTitle'>Monthly Active Users</h3>
+            <h3 className='chartTitle'>{ title }</h3>
             <ResponsiveContainer width="100%" aspect={ 5 / 2 }>
                 <LineChart
-                    width={1000}
-                    height={400}
-                    data={ACTIVE_USER_COUNT}
-                    margin={{
-                        top: 10,
-                        right: 20,
-                        left: 20,
-                        bottom: 10,
-                    }} >
+                    width={width}
+                    height={height}
+                    data={data}
+                    margin={chartMargin} >
                     <CartesianGrid strokeDasharray="5 5" />
-                    <XAxis dataKey="month" />
+                    <XAxis dataKey={xAxisDataKey} />
                     <Tooltip />
-                    <Line type="monotone" dataKey="Active User" stroke="#8884d8" strokeWidth={2} />
+                    <Line type="monotone" dataKey={lineDataKey} stroke="#8884d8" strokeWidth={2} />
                 </LineChart>    
             </ResponsiveContainer>
         </div>
     );
 }
   
+
+LineCharts.defaultProps = {
+    title: "Default LineChart Title",
+    width: 100,
+    height: 100,
+    data: null,
+    lineDataKey: null,
+    xAxisDataKey: null
+}
+
+LineCharts.propTypes = {
+    title: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    data: PropTypes.array,
+    lineDataKey: PropTypes.string,
+    xAxisDataKey: PropTypes.string
+}
