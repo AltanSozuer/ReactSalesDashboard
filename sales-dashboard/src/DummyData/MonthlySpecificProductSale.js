8765aa53f817343ee438d17f7153caa1c14e7fa1
Altan SOZUER <sozueraltan@gmail.com>
2022-12-24 22:04:53 +0300

    Dummy data of product table and its css file is implemented

---
+++ sales-dashboard/src/DummyData/MonthlySpecificProductSale.js
@@ -0,0 +1,50 @@
+export const MonthlySpecificProductSale = [
+    {
+        "month": "Jun",
+        "Rate of Sale": 9
+    },
+    {
+        "month": "Jul",
+        "Rate of Sale": 2
+    },
+    {
+        "month": "Aug",
+        "Rate of Sale": 0
+    },
+    {
+        "month": "Sep",
+        "Rate of Sale": 15
+    },
+    {
+        "month": "Oct",
+        "Rate of Sale": 22
+    },
+    {
+        "month": "Nov",
+        "Rate of Sale": 36
+    },
+    {
+        "month": "Dec",
+        "Rate of Sale": 48
+    },
+    {
+        "month": "Jan",
+        "Rate of Sale": 24
+    },
+    {
+        "month": "Feb",
+        "Rate of Sale": 35
+    },
+    {
+        "month": "Mar",
+        "Rate of Sale": 28
+    },
+    {
+        "month": "Apr",
+        "Rate of Sale": 16
+    },
+    {
+        "month": "May",
+        "Rate of Sale": 12
+    }
+]
